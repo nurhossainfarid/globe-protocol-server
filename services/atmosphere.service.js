bc514ce47452fe26c08c45f6312a9c82c2026822
nurhossainfarid <faahsan1516@gmail.com>
2024-09-23 09:02:20 +0600

    service phase done

---
+++ services/atmosphere.service.js
@@ -0,0 +1,31 @@
+const Atmosphere = require('../models/Atmosphere.model');
+
+// create new soil data
+exports.addAirService = async (data) => {
+    const result = await Atmosphere.create(data);
+    return result;
+}
+
+// get All air
+exports.getAllAirService = async () => {
+    const result = await Atmosphere.find({});
+    return result;
+}
+
+// get one air by id
+exports.getAirByIdService = async (airId) => {
+    const result = await Atmosphere.findOne({ _id: airId });
+    return result;
+}
+
+// update air by Id
+exports.updateAirService = async (airId, data) => {
+    const result = await Atmosphere.updateOne({ _id: airId }, data);
+    return result;
+};
+
+// delete air by id
+exports.deleteAirByIdService = async (airId) => {
+    const result = await Atmosphere.deleteOne({ _id: airId });
+    return result;
+}
---
+++ services/hydrosphere.service.js
@@ -0,0 +1,31 @@
+const Hydrosphere = require('../models/Hydrosphere.model');
+
+// create new soil data
+exports.addPondService = async (data) => {
+    const result = await Hydrosphere.create(data);
+    return result;
+}
+
+// get All soil
+exports.getAllPondService = async () => {
+    const result = await Hydrosphere.find({});
+    return result;
+}
+
+// get one soil by id
+exports.getPondByIdService = async (pondId) => {
+    const result = await Hydrosphere.findOne({ _id: pondId });
+    return result;
+}
+
+// update soil by Id
+exports.updatePondService = async (pondId, data) => {
+    const result = await Hydrosphere.updateOne({ _id: pondId }, data);
+    return result;
+};
+
+// delete soil by id
+exports.deletePondByIdService = async (pondId) => {
+    const result = await Hydrosphere.deleteOne({ _id: pondId });
+    return result;
+}
